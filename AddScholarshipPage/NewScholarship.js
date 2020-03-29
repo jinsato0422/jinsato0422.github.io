01945cc037fa0355aeb422de9371bd91ca294102
joweriae <56801882+joweriae@users.noreply.github.com>
2020-03-29 14:53:12 -0600

    Add files via upload
    jss

---
+++ AddScholarshipPage/NewScholarship.js
@@ -0,0 +1,21 @@
+var id
+const form = document.querySelector('#addscholarship');
+
+//Saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('Scholarship Database').doc(form.d_id.value).set({
+        name: form.name.value,
+        provider: form.provider.value,
+        value: form.value.value,
+        numberAvailable: form.students.value,
+        category: form.category.value,
+        offered: form.offered.value,
+        courseLoad: form.courseLoad.value,
+        fieldOfStudy: form.fieldOfStudy.value,
+        yearOfStudy: form.yearOfStudy.value,
+        description: form.description.value,
+        levelOfStudy: form.levelOfStudy.value,
+
+    })
+})
