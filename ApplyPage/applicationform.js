8718c06e0a628a454d125032ead03bd5e9397bfd
joweriae <56801882+joweriae@users.noreply.github.com>
2020-04-13 15:20:15 -0600

    Add files via upload

--- ApplyPage/applicationform.js
+++ ApplyPage/applicationform.js
@@ -14,43 +14,47 @@ function fileUpload(event) {
     var upFileSD = document.getElementById("otherdocs").files[0]
     supdocRef.put(upFileSD);
 
-    window.location.href = "../Homepage/Homepage.html?"
 }
-
 var scholarshipSelectionPane = document.getElementById("availableScholarships");
 
-function findScholarshipInformation() {
-    availableScholarships = new Array();
-    database.collection('Scholarship Database').get().then((snapshot) => {
+function renderScholarship(doc) {
+    let opt = document.createElement('option')
+    opt.textContent = doc.data().name;
+    scholarshipSelectionPane.add(opt);
+}
+db.collection('Scholarship Database').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderScholarship(doc)
+    })
+})
+var scholarshipID = []
+var scholarshipsSelected = []
+scholarshipsSelected = $('#availableScholarships').val();
+db.collection('Scholarship Database').get().then((snapshot) => {
         snapshot.docs.forEach(doc => {
-            addScholarshipInformation(doc);
+
+            if (scholarshipsSelected.includes(doc.data().name)) {
+                scholarshipID.push(doc.id)
+            }
         })
     })
-}
+    //Saving data
+const waitScholarship = ms => new Promise(resolve => setTimeout(resolve, ms));
+waitScholarship(0.5 * 1000).then(() => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        db.collection('applications').doc(form.s_id.value).set({
+            scholarshipsSelected: scholarshipID,
+            fname: form.fname.value,
+            lname: form.lname.value,
+            email: form.email.value,
+            number: form.number.value,
+            s_id: form.s_id.value,
+            gpa: form.gpa.value,
+            dob: form.dob.value,
+            degree: form.degree.value,
+            grades_b: form.grades.value,
+        })
 
-function addScholarshipInformation(scholarship) {
-    var scholarshipName = scholarship.data().name;
-    addToScreen(scholarshipName, scholarshipSelectionPane);
-}
-
-function addToScreen(toAdd, whereToAdd) {
-    var opt = document.createElement("option");
-    opt.text = toAdd;
-    whereToAdd.add(opt);
-}
-//Saving data
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    db.collection('applications').doc(form.s_id.value).set({
-        fname: form.fname.value,
-        lname: form.lname.value,
-        email: form.email.value,
-        number: form.number.value,
-        s_id: form.s_id.value,
-        gpa: form.gpa.value,
-        dob: form.dob.value,
-        degree: form.degree.value,
-        grades_b: form.grades.value,
     })
-
 })
