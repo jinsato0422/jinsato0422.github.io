92933a7c70cf61da1dc538b1b119033c2fc50dec
Jin <36085216+Sekai422@users.noreply.github.com>
2020-04-13 21:51:19 -0600

    added apply and nominate to navbar

--- Homepage/Homepage.js
+++ Homepage/Homepage.js
@@ -47,15 +47,17 @@ function getDatabase(name) {
 
 // Checks if the user is vaild
 function UserDisplay(userType) {
+    var navbarElementsList = document.querySelectorAll('.nav-item');
     if (userType == 'Student') {
-        var navbarElementsList = document.querySelectorAll('.nav-item');
         if (document.querySelector('#apply') != null) {
             document.querySelector('#apply').setAttribute("onclick", "window.location.href ='../ApplyPage/applicationform.html?" + findUser() + "';");
         }
+
+        navbarElementsList[2].classList.remove("d-none");
+        navbarElementsList[2].classList.remove("disabled");
         navbarElementsList[3].classList.remove("d-none");
         navbarElementsList[3].classList.remove("disabled");
     } else if (userType == 'Coordinator') {
-        var navbarElementsList = document.querySelectorAll('.nav-item');
         console.log("This is the list");
         console.log(navbarElementsList);
         var totalLength = navbarElementsList.length;
@@ -71,7 +73,8 @@ function UserDisplay(userType) {
             document.querySelector('#apply').classList.add('disabled');
         }
     } else if (userType == 'Professor') {
-
+        navbarElementsList[5].classList.remove("d-none");
+        navbarElementsList[5].classList.remove("disabled");
     } else {
         alert('It seems that we detected that you are not an authorized user\nRedirecting you to Login Page');
         window.location.href = "../LoginPage/login.html";
@@ -83,10 +86,12 @@ function InitHomepage() {
     var navbarElementsList = document.querySelectorAll('.nav-link');
     navbarElementsList[0].setAttribute('href', "../HomePage/HomePage.html?" + findUser());
     navbarElementsList[1].setAttribute('href', "../ScholarshipPage/ScholarshipPage.html?" + findUser());
-    navbarElementsList[2].setAttribute('href', "../StatisticsPage/StatisticsPage.html?" + findUser());
-    navbarElementsList[3].setAttribute('href', "../AcceptancePage/AcceptancePage.html?" + findUser());
-    navbarElementsList[4].setAttribute('href', "../AddScholarshipPage/NewScholarship.html?" + findUser());
-    navbarElementsList[5].setAttribute('href', "../PickRecipientPage/pickRecipient.html?" + findUser());
+    navbarElementsList[2].setAttribute('href', "../ApplyPage/applicationform.html?" + findUser());
+    navbarElementsList[3].setAttribute('href', "../StatisticsPage/StatisticsPage.html?" + findUser());
+    navbarElementsList[4].setAttribute('href', "../AcceptancePage/AcceptancePage.html?" + findUser());
+    navbarElementsList[5].setAttribute('href', "../ApplyPage/nomination.html?" + findUser());
+    navbarElementsList[6].setAttribute('href', "../AddScholarshipPage/NewScholarship.html?" + findUser());
+    navbarElementsList[7].setAttribute('href', "../PickRecipientPage/pickRecipient.html?" + findUser());
     console.log(document.querySelector('#apply'));
 
     if (document.querySelector('#learn') != null) {
